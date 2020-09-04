49cdf22ef55b4b29554d18d7d5cd860cfaa89b81
David Sherlock <davidsherlock163@hotmail.com>
2020-09-04 11:35:01 +0100

    Add project files.

---
+++ udemycourse/udemycourse/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace udemycourse
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void saveButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ udemycourse/udemycourse/UniversityPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace udemycourse
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UniversityPage : ContentPage
+    {
+        public UniversityPage()
+        {
+            InitializeComponent();
+        }
+
+        private void newContextToolbarItem_clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+
+        }
+    }
+}
