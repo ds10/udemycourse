2c79e105a9cea30a51664e86acefc1ad6c8183ac
David Sherlock <davidsherlock163@hotmail.com>
2020-09-11 16:43:44 +0100

    finished coursem added sql list

--- udemycourse/udemycourse/UniversityPage.xaml.cs
+++ udemycourse/udemycourse/UniversityPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Universities.Classes;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -20,7 +21,21 @@ namespace udemycourse
         private void newContextToolbarItem_clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new MainPage());
-
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using(SQLiteConnection conn = new SQLiteConnection (App.FilePath))
+            {
+                conn.CreateTable<University>();
+                var universities = conn.Table<University>().ToList();
+                contactsListView.ItemsSource = universities;
+
+            }
+        }
+
+
     }
 }
